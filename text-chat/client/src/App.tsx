6f1d5e8aa1d188b6d9932c6b4595ee8b21012684
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-01-10 22:46:41 -0500

    chage: ws to http

--- text-chat/client/src/App.tsx
+++ text-chat/client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react'
 import { io } from 'socket.io-client'
 
-const socket = io('ws://localhost:4000')
+const socket = io('http://localhost:4000')
 
 export function App() {
   let textareaE = useRef<HTMLTextAreaElement>(null)
