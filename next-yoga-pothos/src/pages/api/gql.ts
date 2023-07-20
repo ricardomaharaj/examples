d30dc7f00371d5178fa984a9f39fff4db7d0bfdd
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2023-07-20 00:17:27 -0400

    remove example

--- next-yoga-pothos/src/pages/api/gql.ts
+++
@@ -1,9 +0,0 @@
-import { createYoga } from 'graphql-yoga'
-import { schema } from '~/server/schema'
-import { type PageConfig } from 'next'
-
-export const config: PageConfig = { api: { bodyParser: false } }
-
-const yoga = createYoga({ schema, graphqlEndpoint: '/api/gql' })
-
-export default yoga
