1d9fa7f086ef25bf5a15b250647df59097072405
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2025-07-12 19:27:07 -0400

    remove import

--- mantine-rtf-tailwind/src/pages/_app.tsx
+++ mantine-rtf-tailwind/src/pages/_app.tsx
@@ -2,12 +2,9 @@ import type { AppProps } from "next/app"
 
 import "~/styles/globals.css"
 
-/* do not import mantine styles, we style it ourselves */
+/* do not import mantine styles, we style it ourselves in the globals css file */
 // import "@mantine/core/styles.css"
 // import "@mantine/tiptap/styles.css"
-
-/* our custom mantine rtf styles */
-import "~/styles/mantine-rtf.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
